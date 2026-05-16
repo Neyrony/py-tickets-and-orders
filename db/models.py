c84524b5beb347e2823b62ea1d4f21f68679285a
Neyrony <sserebriy@gmail.com>
2026-05-16 16:25:39 +0300

    fixed flake

--- db/models.py
+++ db/models.py
@@ -64,7 +64,8 @@ class MovieSession(models.Model):
 
 class Order (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="users")
+    user = models.ForeignKey(AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name="users")
 
     def __str__(self) -> str:
         return str(self.created_at)
