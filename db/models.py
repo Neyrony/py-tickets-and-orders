b5646c5b9175d6ea28486e1a68c5e294091b3574
Neyrony <sserebriy@gmail.com>
2026-05-16 16:06:02 +0300

    Implemented solution for py-tickets-and-orders

--- db/models.py
+++ db/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+from settings import AUTH_USER_MODEL
 
 
 class Genre(models.Model):
@@ -21,6 +26,11 @@ class Movie(models.Model):
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["title"], name="title_index"),
+        ]
 
     def __str__(self) -> str:
         return self.title
@@ -50,3 +60,68 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(
+        to=MovieSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(
+        to=Order, on_delete=models.CASCADE, related_name="tickets"
+    )
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self) -> str:
+        return (f"{self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
+                f"(row: {self.row}, "
+                f"seat: {self.seat})")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["row", "seat", "movie_session"],
+                name="unique_ticket",
+            )
+        ]
+
+    def clean(self) -> None:
+        super().clean()
+        if self.row < 0 or self.row > self.movie_session.cinema_hall.rows:
+            raise ValidationError(
+                {"row":
+                    [
+                        f"row number must be in available range: "
+                        f"(1, rows): "
+                        f"(1, {self.movie_session.cinema_hall.rows})"
+                    ]}
+            )
+        elif (self.seat < 0
+              or self.seat > self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError(
+                {"seat": [f"seat number must be in available range: "
+                          f"(1, seats_in_row): "
+                          f"(1, "
+                          f"{self.movie_session.cinema_hall.seats_in_row})"]})
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+class User(AbstractUser):
+    email = models.EmailField(blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, default="")
+    last_name = models.CharField(max_length=255, blank=True, default="")
